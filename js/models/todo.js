ebeeecac83c22cbbdf8b4b243cf40cf84a5b25c8
howardjmn <semiles@msn.com>
2017-07-01 16:32:03 -0500

    Implement priority selection on save

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,26 +1,45 @@
 /*global Backbone */
 var app = app || {};
 
-(function () {
+(function ()
+{
 	'use strict';
 
 	// Todo Model
 	// ----------
 
 	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
-	app.Todo = Backbone.Model.extend({
+	app.Todo = Backbone.Model.extend
+    ({
 		// Default attributes for the todo
 		// and ensure that each todo created has `title` and `completed` keys.
-		defaults: {
+		defaults:
+        {
 			title: '',
-			completed: false
+			completed: false,
+            maxPriority: 2,
+            lowPriority: 0,
+            normalPriority: 1,
+            highPriority: 2,
+            priority: 1
 		},
 
 		// Toggle the `completed` state of this todo item.
-		toggle: function () {
-			this.save({
+		toggle: function ()
+        {
+			this.save
+            ({
 				completed: !this.get('completed')
 			});
-		}
+		},
+
+        // Toggle the `priority` state of this todo item.
+        togglePriority: function ()
+        {
+            this.save
+            ({
+                priority: this.get('priority') + 1
+            });
+        }
 	});
 })();
